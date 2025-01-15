bcbb9be70508c9349306bcb44d253bf15182a854
Ahid-604 <alnmyahdlybdalqadrhady@gmail.com>
2025-01-15 19:17:43 +0300

    Delete ahid.dart

--- ahid.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  String myDetails = '''
-MY NAME : Ahid Ali Hadi.
-rigistration Number : 52222207.
-Specialixation : Infromation tecnology.
-Level : Level 3;
-''';
-print(myDetails);
-}
