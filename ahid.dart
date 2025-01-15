dad39cf0fa5920cd81ea864ca3a858bac2baabd1
Ahid-604 <alnmyahdlybdalqadrhady@gmail.com>
2025-01-15 19:16:14 +0300

    Add files via upload

---
+++ ahid.dart
@@ -0,0 +1,9 @@
+void main(){
+  String myDetails = '''
+MY NAME : Ahid Ali Hadi.
+rigistration Number : 52222207.
+Specialixation : Infromation tecnology.
+Level : Level 3;
+''';
+print(myDetails);
+}
